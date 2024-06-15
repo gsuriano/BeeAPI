2e415a752cfa90f1503e15c1d11db94d6140a61d
gsuriano <gisuriano@gmail.com>
2024-06-15 14:47:14 +0200

    Docker compose for local test with promethues metrics, Used KIND to deploy the app and Prometheus using ingress-Nginx First part phase 4

--- Dockerfile
+++ Dockerfile
@@ -10,7 +10,6 @@ EXPOSE 5000
 
 # Define environment variable
 ENV FLASK_APP=main.py
-ENV URL_API="https://api.opensensemap.org/boxes/"
 
 # Run app.py when the container launches
 CMD ["flask", "run", "--host=0.0.0.0"]  
--- main.py
+++ main.py
@@ -2,16 +2,34 @@
 from datetime import datetime, timedelta
 from typing import List
 import os
+from prometheus_client import (generate_latest,
+                               REGISTRY,
+                               CONTENT_TYPE_LATEST,
+                               Counter,
+                               Summary,
+                               Histogram
+                        )
+from dotenv import load_dotenv
 import flask
 import requests
 
+load_dotenv()
 app = flask.Flask(__name__)
 
+# Create a metric to track time spent and requests made.
+# These are custom metrics defined by you.
+REQUEST_TIME = Summary('request_processing_seconds', 'Time spent processing request')
+REQUEST_COUNTER = Counter('request_count', 'Total number of requests')
+REQUEST_HISTOGRAM = Histogram('request_latency_seconds',
+                              'Request latency',
+                              buckets=[0.3, 1.5, 3.0, 10.5])
+
 @app.route("/version")
+@REQUEST_TIME.time()
 def version() -> str:
     """Function printing BeeAPI version."""
-    with open("version.txt", encoding="utf-8") as f:
-        return f.read()
+    REQUEST_COUNTER.inc()
+    return flask.Response(os.getenv("version"))
 
 def validate(response: dict, key: str) -> List[float]:
     """
@@ -26,14 +44,18 @@ def validate(response: dict, key: str) -> List[float]:
             if sensor["title"] == key:
                 sensor_url = url + sensebox["_id"]+"/sensors/"+sensor["_id"]
                 measurement = requests.get(sensor_url,timeout=600).json()
-                if measurement['lastMeasurement'] is not None:
+                if measurement.get('lastMeasurement') is not None:
                     measurement_time_str = measurement['lastMeasurement']['createdAt']
                     measument_time = datetime.strptime(
                         measurement_time_str,
                         "%Y-%m-%dT%H:%M:%S.%fZ"
                     )
+
+                    # Sensor save the time of recording with another timezone
+                    # so we need to transpose by two hours
+                    measument_time = measument_time + timedelta(hours=2)
                     if measument_time > now - timedelta(hours=1) :
-                        res.append(measurement['lastMeasurement']['value'])
+                        res.append(float(measurement['lastMeasurement']['value']))
         if len(res) == 3:
             break
     if len(res) < 3:
@@ -41,13 +63,14 @@ def validate(response: dict, key: str) -> List[float]:
     return res
 
 @app.route("/temperature")
+@REQUEST_TIME.time()
 def temperature():
     """ 
     Return current average temperature based on all senseBox data.
     Ensure that the data is no older 1 hour.
     """
-    with open("ids.txt", encoding="utf-8") as f:
-        central_id = f.readline().strip()
+    start_time = datetime.now()
+    central_id = os.getenv("senseBoxId")
     url = os.getenv("URL_API")
 
     response = requests.get(url + central_id, timeout=600).json()
@@ -61,13 +84,39 @@ def temperature():
     temperatures = validate(response,"Temperatur")
 
     if temperatures is None:
+        REQUEST_COUNTER.inc()
+        latency = (datetime.now() - start_time).total_seconds()
+        REQUEST_HISTOGRAM.observe(latency)
         return "Data older than 1 hour whitin 5000 meters"
-    return sum(temperatures) / len(temperatures)
+    avg = sum(temperatures) / len(temperatures)
+    if avg <= 10:
+        status = "Too cold"
+    elif avg <= 36 and avg > 10:
+        status = "Good"
+    elif avg > 36:
+        status = "Too hot"
+    REQUEST_COUNTER.inc()
+    latency = (datetime.now() - start_time).total_seconds()
+    REQUEST_HISTOGRAM.observe(latency)
+    return flask.jsonify({
+        "avg": avg,
+        "status": status
+        })
+
+@app.route("/metrics")
+@REQUEST_TIME.time()
+def metrics():
+    """
+    Returns default Prometheus metrics about the app.
+    """
+    return flask.Response(generate_latest(REGISTRY), mimetype=CONTENT_TYPE_LATEST)
 
 @app.route("/")
+@REQUEST_TIME.time()
 def index() -> str:
     """Function printing index."""
-    return "Hello World!"
+    REQUEST_COUNTER.inc()
+    return flask.Response("Hello World!")
 
 if __name__ == "__main__":
     app.run()
